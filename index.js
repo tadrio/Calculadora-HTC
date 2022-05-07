dd884fbcf4fa320ab22b478930ce9afa1be4fec3
Tadrio Tabosa <tadrio_88@hotmail.com>
2022-05-07 19:19:17 -0300

    operation
    still have some bugs, but it's fully functional

--- index.js
+++ index.js
@@ -1,21 +1,121 @@
+//-------------------------CONSTANTES---------------------------------
 const $buttons = document.querySelectorAll(".calc-button");
 const $display = document.querySelector(".calculator__display-input");
 const $result = document.querySelector(".calculator__display-result");
-const $equals = document.querySelector(".calc-button-equal");
-const $calculate = document.querySelector(".operations");
+const $clear = document.querySelector(".clear-button");
+const $operations = document.querySelectorAll(".operation-button");
+const $equal = document.querySelector(".calc-button-equal");
+const $signal = document.querySelector(".signal-button");
+const $percent = document.querySelector(".percent-button");
+const $callback = document.querySelector(".calculator__display-history");
+const $closeCallback = document.querySelector(".calculator__callback__close-button");
+const $callbackWindow = document.querySelector(".calculator__callback");
+const $callbackOperation = document.querySelector(".calculator__callback__operation")
+const $callbackResult = document.querySelector(".calculator__callback__result")
 
-let finalResult = 0;
-let a = '';
-let calculate = "";
-console.log;
+//--------------------------VARIÁVEIS---------------------------------
+let n1 = "";
+let n2 = "";
+let operation = false;
+let previous = "";
+let firstSignal = false;
+let callbackResult = "";
+let callbackOperation = "";
 
+//----------------------------FUNÇÕES---------------------------------
+function result() {
+  if (operation) {
+    callbackOperation = (n1 + " " + previous + " " + n2);
+    if (previous == "+") {
+      n1 = Number(n1) + Number(n2);
+    }
+    if (previous == "-") {
+      n1 = Number(n1) - Number(n2);
+    }
+    if (previous == "÷") {
+      n1 = Number(n1) / Number(n2);
+    }
+    if (previous == "×") {
+      n1 = Number(n1) * Number(n2);
+    }
+    callbackResult = n1
+    operation = false;
+    n2 = "";
+  }
+}
+
+function displayResult(){
+  result();
+  $result.textContent = n1;
+  $display.value = n1;
+}
+
+//----------------------------BOTÕES----------------------------------
+//BOTÃO CLEAR (limpa toda a calculadora)
+$clear.addEventListener("click", function () {
+  $display.value = "";
+  $result.textContent = 0;
+  //lembrar de zerar TODAS as variáveis
+  n1 = "";
+  n2 = "";
+  operation = false;
+  previous = "";
+  firstSignal = false;
+});
+
+//BOTÕES DE NÚMERO
 $buttons.forEach(function ($button) {
   $button.addEventListener("click", function () {
-    calculate = calculate + $button.textContent;
-    $display.value = calculate;
+    if (!operation) {
+      n1 = n1 + $button.textContent;
+    } else {
+      n2 = n2 + $button.textContent;
+    }
+    $display.value = $display.value + $button.textContent;
   });
 });
 
-$equals.addEventListener("click", function () {
+//BOTÕES DE OPERAÇÃO
+$operations.forEach(function ($button) {
+  $button.addEventListener("click", function () {
+    result();
+    previous = $button.textContent;
+    $result.textContent = n1;
+    operation = !operation;
+    $display.value = $display.value + $button.textContent;
+  });
+});
 
+//BOTÃO DE IGUALDADE
+$equal.addEventListener("click", function () {
+  displayResult();
+});
+
+//BOTÃO DE PORCENTAGEM
+$percent.addEventListener("click", function () {
+  result();
+  n1 = n1/100;
+  displayResult();
+});
+
+//BOTÃO DE TROCA DE SINAL
+$signal.addEventListener("click", function () {
+  if (!firstSignal) {
+    n1 = -n1;
+    $result.textContent = n1;
+    $display.value = n1;
+    firstSignal = true;
+  }
+  n2 = -n2;
+  displayResult();
+});
+
+$callback.addEventListener("click", function(){
+  $callbackWindow.classList.toggle('closed');
+  $callbackOperation.textContent = callbackOperation;
+  $callbackResult.textContent = callbackResult;
+  });
+
+$closeCallback.addEventListener("click", function(){
+  $callbackWindow.classList.toggle('closed');
 });
